7ea5e33afd32a3117232114878ee11ab7af41fc6
kazuhide maeda <kzhd15med3g@gmail.com>
2014-04-26 19:35:40 +0900

    added struct jail

--- ext/prison/prison.c
+++ ext/prison/prison.c
@@ -1,5 +1,27 @@
 #include "prison.h"
 
+VALUE cPrison;
+
+struct jail {
+				VALUE	version;
+				VALUE	path;
+				VALUE	hostname;
+				VALUE	jailname;
+				VALUE ip4s;
+				VALUE	ip6s;
+				VALUE	ip4;
+				VALUE	ip6;
+};
+
+VALUE list_func(VALUE self){
+				puts ("Prison#list()");
+				return self;
+}
+
 void Init_prison(){
 				puts("called Init_prison()");
+				cPrison = rb_define_class("Prison", rb_cObject);
+
+				rb_define_method(cPrison, "list", RUBY_METHOD_FUNC(list_func),0);
 }
+
